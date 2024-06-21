89dd60cdfa158d1c082f3a0683599fbacf14cd9e
Sampath NM <Sampath.Narayana.Murthy.ExternalContractor@sg.ey.com>
2024-06-21 13:29:02 +0800

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TestAzureDepSlots.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
